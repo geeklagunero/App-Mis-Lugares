26e5bd6d7a68d96a4fa0f2ed6668137b08d90054
Ricardo <ricardomon85@gmail.com>
2022-05-09 18:17:09 -0500

    Convertir a geocodificacion

--- misLugares/LocationActualViewController.swift
+++ misLugares/LocationActualViewController.swift
@@ -17,6 +17,19 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
     @IBOutlet weak var botonEtiqueta: UIButton!
     @IBOutlet weak var buttonObtener: UIButton!
     
+    //objeto que realizara la geocodificacion
+    let geocoder = CLGeocoder()
+    //lugar para marcar
+    //Objeto que contiene los resultado de la direcccion
+    //es opcional porque no tendra valor cuando aun no aya ubicacion o cuando la ubicacion no corresponda a una direccion de calle
+    var placemark: CLPlacemark?
+    //Realización de geocodificación inversa
+    //y cambuia a true cuando se lleva a cabo una operacion de geolocalizacion
+    // asi solo hacemo una solicutud ala ves si esta en true es que esta ocupado
+    var performingReverseGeocoding = false
+    var lastGeocodingError: Error?
+
+    
     //objeto que me dara las coordenadas del gps
     let locationManager = CLLocationManager()
     
@@ -24,7 +37,7 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
     var location: CLLocation?
     //actulizando ubicacion en falso
     var updatingLocation = false
-    //ultimo herror de ubicacion
+    //ultimo error de ubicacion
     var lastLocationError: Error?
 
     override func viewDidLoad() {
@@ -33,6 +46,15 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
         // Do any additional setup after loading the view.
     }
 
+    func configureGetButton(){
+        //si la variabla updatingLocation es versadeda es decir el manejador de ubicacion esta funcionando
+        //cambiamos el titutlo de botn a parar ubicacion si no le ponemos obtener ubicacion
+        if updatingLocation {
+            buttonObtener.setTitle("stop obetner Ubicacion", for: .normal)
+        } else {
+            buttonObtener.setTitle("Obtener mi uubicacion", for: .normal)
+        }
+    }
     
     // MARK: - Actions
     @IBAction func obtenerUbicacion() {
@@ -51,7 +73,18 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
             return
         }
         
-        iniciarManejadorUbicacion()
+        //si authStatus esta como aprobado y/o habilitado ejecutamos el if
+        //el if nos dice si la variable updatingLocation es verdadera quiere decir que el manejador de ubicaicon esta funcionando
+        //entonces lo paramos
+        // si no esque el manejador esta detenenido y empezamos a localizar y ponemos a nil la locacion que es nuestra ubicacion buena y el error
+        if updatingLocation {
+            pararManejadorUbicacion()
+        } else {
+            self.location = nil
+            self.lastLocationError = nil
+            iniciarManejadorUbicacion()
+        }
+        
         actulizarLabels()
     }
     
@@ -81,12 +114,20 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
                 } else if !CLLocationManager.locationServicesEnabled() {
                   statusMessage = "Location Services Disabled"
                 } else if updatingLocation {
+                    //si updatingLocation es verdadera se pone buscando
+                    //porque el manejador de ubicacion esta funcionando
+                    //y no hay errores
+                    //y depues ya bva buscar el pimrer objeto de la ubicacion
                   statusMessage = "Searching..."
                 } else {
+                    //si la variable es falsa poenmos este mensaje
+                    //porque el manejador de ubicacion no esta funcionando
                   statusMessage = "Tap 'Get My Location' to Start"
                 }
             labelMensaje.text = statusMessage
         }
+        //llamamos al metodo de configurar boton
+        configureGetButton()
     }
     
     func mostrarAlertaDelServicioUbicacion() {
@@ -124,10 +165,16 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Fallo con el error \(error.localizedDescription)")
         
+        //verifica si el error es de unibicacion desconocida
+        //que para ese error lo seguira intentando por eso salimos de la
+        //ejecucion del metodo con el return
         if (error as NSError).code == CLError.locationUnknown.rawValue {
             return
         }
         
+        //en caso de un arror mas gradave y no entre al if
+        //almacenara el error en la variable laslocation
+        // y parara el manejador de ubicacion y actulizara los label para mostrar el error
         lastLocationError = error
         pararManejadorUbicacion()
         actulizarLabels()
@@ -139,9 +186,58 @@ class LocationActualViewController: UIViewController, CLLocationManagerDelegate 
         let nuevaUbicacion = locations.last!
         print("Actulizo ubicaciones con la ultima ubicacion es \(nuevaUbicacion)")
         
-        self.location = nuevaUbicacion
-        self.lastLocationError = nil
+        //1.-Aqui ignoara las ubicaciones almacenas en cache si son antiguas y salimos del metodo con el return
+        //y se vuelve a retomar una lectura valida
+        if nuevaUbicacion.timestamp.timeIntervalSinceNow < -5 {
+            return
+        }
+        
+        //2.-si la presion horizontal son negativas se ingnoran y se sale del metodo y se veulve a retomar una lectura valida
+        if nuevaUbicacion.horizontalAccuracy < 0 {
+            return
+        }
+        
+        //3.-si es la primera lectura de la ubiacion location == nil o la nueva ubicacion es mas presisa que la anterior
+        //entramos al if para ejecutar el paso 4 si no no salimos y se vuvle a hacer una lectura valida
+        if self.location == nil || self.location!.horizontalAccuracy > nuevaUbicacion.horizontalAccuracy {
+            //4.-//borrar cualquier error anterios y almcena la nueva ubicacion mas presisa en la ubicacion global
+            self.lastLocationError = nil
+            self.location = nuevaUbicacion
+            
+            //5.-si la presion de la nueva ubicacion es mejor que la presion deseada o iguual paramos el manejador de ubicacion y giardamos esa ubicacion
+            if nuevaUbicacion.horizontalAccuracy <= self.locationManager.desiredAccuracy {
+                print("*** We are done")
+                pararManejadorUbicacion()
+            }
+        }
+        
+//        self.location = nuevaUbicacion
+//        self.lastLocationError = nil
         actulizarLabels()
+        
+        //vemos si esta en false es que no esta haciendo ninfuna solicitud a lso servios de apple y podemos empesar
+        //por el otro lado si es true quiere decir que esta ocupado por eso dentro del cierre los cambiamos a true
+        //y asi esta ocupado
+        if !self.performingReverseGeocoding {
+            print("*** Going to geocoder  vammos a geocodificar la inversa")
+            //esta ocupado
+            self.performingReverseGeocoding = true
+            
+            
+            self.geocoder.reverseGeocodeLocation(nuevaUbicacion) { placemarks, error in
+                if let error = error {
+                    print("Error de geocodificacion inverso \(error.localizedDescription)")
+                    return
+                }
+                if let placers = placemarks {
+                    print("*** lugares encontrados \(placers) ")
+                }
+            }//fin del geocoder
+        }//fin del if de gecodificacion inversa
     }
+    
+    
+    
+    
 }
 
